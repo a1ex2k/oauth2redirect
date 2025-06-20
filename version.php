97de4a2803a79ebce19d8e9cfceaaf35d699974b
Aleksey Ananko <41271319+a1ex2k@users.noreply.github.com>
2025-06-20 11:03:27 +0300

    fix wantsurl

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'auth_oauth2redirect';
-$plugin->version   = 2025061602;
+$plugin->version   = 2025062002;
 $plugin->requires  = 2022112800;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v1.0';
+$plugin->release   = 'v1.0.1';
